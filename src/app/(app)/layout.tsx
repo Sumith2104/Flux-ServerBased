b3e24eaa2c4c444117f1d5d51af6664a160d98bd
Sumith U <sumithsumith4567890@gmail.com>
2025-09-09 13:34:07 +0000

    y i cant select project from drop down

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -14,6 +14,7 @@ import { cn } from "@/lib/utils";
 import { loginAction, logoutAction } from "./actions";
 import { Skeleton } from "@/components/ui/skeleton";
 import Cookies from "js-cookie";
+import { redirect } from "next/navigation";
 
 export default function AppLayout({ 
     children,
